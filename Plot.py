44c9d6622cf364af789d9f0cb1e839a663ede306
Zizheng Wu <me@zizhengwu.com>
2016-04-01 03:18:19 -0400

    data from csv and numpy functions example

--- Plot.py
+++ Plot.py
@@ -6,10 +6,18 @@ from itertools import cycle
 class Plot:
     def __init__(self, **arg):
         fig1 = plt.figure()
-        styles = lines.lineStyles.keys()
-        linecycler = cycle(styles)
-        for data in arg['data']:
-            plt.plot(data['x'], data['y'], next(linecycler), label=data['label'])
+        dot_styles = ['b--', 'gs', 'r^', 'cp', 'm*'].__iter__()
+        line_styles = ['b-', 'g-', 'r-', 'c-', 'm-'].__iter__()
+        for data in arg['data']['dots']:
+            plt.plot(data['x'], data['y'], next(dot_styles), label='Bloom filter table size = ' + str(data['label']))
+        for data in arg['data']['lines']:
+            plt.plot(data['x'], data['y'], next(line_styles), label='Bloom filter table size = ' + str(data['label'] + ' theoretical'))
+        axes = plt.gca()
+        if 'xlimit' in arg:
+            axes.set_xlim(arg['xlimit'])
+        if 'ylimit' in arg:
+            axes.set_ylim(arg['ylimit'])
         plt.xlabel(arg['xlabel'])
         plt.ylabel(arg['ylabel'])
+        plt.legend(loc=4)
         plt.show()
--- experiment.py
+++ experiment.py
@@ -1,5 +1,27 @@
 from Plot import Plot
+import numpy
 
 
 if __name__ == '__main__':
-    plot = Plot(data=[{'x': [1, 2], 'y': [3, 4], 'label': '1'}], xlabel='xtest', ylabel='ytest')
+    data = {}
+    for filename in ['256.txt', '512.txt', '1024.txt', '2048.txt', '4096.txt']:
+        int_name = int(filename.split('.')[0])
+        data[int_name] = {}
+        data[int_name]['x'] = []
+        data[int_name]['y'] = []
+        with open('data/' + str(filename), 'r') as lines:
+            for line in lines:
+                data[int_name]['x'].append(int(line.split('\t')[0].strip()))
+                data[int_name]['y'].append(float(line.split('\t')[1].strip()))
+
+    to_be_ploted_dots = []
+    for k, v in sorted(data.items()):
+        to_be_ploted_dots.append({'x': v['x'], 'y': v['y'], 'label': k})
+
+    x = numpy.linspace(1, 4000, 4000)
+    y = [numpy.exp(numpy.log(2)*numpy.log(2)*-(256/x)), numpy.exp(numpy.log(2)*numpy.log(2)*-(512/x)), numpy.exp(numpy.log(2)*numpy.log(2)*-(1024/x)), numpy.exp(numpy.log(2)*numpy.log(2)*-(2048/x)), numpy.exp(numpy.log(2)*numpy.log(2)*-(4096/x))]
+    to_be_ploted_lines = []
+    label = ['256', '512', '1024', '2048', '4096'].__iter__()
+    for v in y:
+        to_be_ploted_lines.append({'x': x, 'y': v, 'label': next(label)})
+    plot = Plot(data={'dots': to_be_ploted_dots, 'lines': to_be_ploted_lines}, xlabel='# of items added into the subset that our Bloom filter is maintaining', ylabel='false positive probability', xlimit=[0, 4000])
